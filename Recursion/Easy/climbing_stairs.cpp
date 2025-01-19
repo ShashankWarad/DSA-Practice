0db7d7b90acf6945f3f1b29dbbd8c3a13f102084
ShashankWarad <105542248+ShashankWarad@users.noreply.github.com>
2025-01-19 19:17:24 +0530

    Create climbing_stairs.cpp

---
+++ Recursion/Easy/climbing_stairs.cpp
@@ -0,0 +1,23 @@
+// Method 1
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0 || n == 1) return 1;
+
+        int one = climbStairs(n-1);
+        int two = climbStairs(n-2);
+
+        return one + two;
+    }
+};
+
+// Method 2
+
+class Sol {
+public: 
+    int climbStairs(int n){
+      if(n <= 3) return n;
+      return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
