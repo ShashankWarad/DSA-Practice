f077f8494957342f7bcdc2135e8c45fd6c2cb0ba
ShashankWarad <105542248+ShashankWarad@users.noreply.github.com>
2025-01-19 20:47:35 +0530

    Create 0_1_knapsack_problem_top_down_approach.cpp

---
+++ Dynamic Programming/Easy/0_1_knapsack_problem_top_down_approach.cpp
@@ -0,0 +1,20 @@
+//  This DP code is for 0 - 1 knapsack problem in top - down approach
+
+int knapsack(int wt[], int val[], int W, int n){
+  int dp[n+1][W+1];
+  for(int i = 0; i<n+1;i++)
+    for(int j = 0; j < W+1; j++)
+      if(i == 0 || j == 0)
+        return dp[i][j] = 0;
+  
+  for(int i = 1; i < n+1; i++)
+    for(int j = 1; j < W+1; j++){
+      if(wt[i-1] <= j){
+        dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+      }
+      else {
+        dp[i][j] = dp[i-1][j];
+      }
+    }
+    return dp[n][W];
+}
