f8f7e84d708ff80222e66f50d1d50e7c19b37f9d
ShashankWarad <105542248+ShashankWarad@users.noreply.github.com>
2025-02-10 17:49:06 +0530

    Create valid_anagram.cpp

---
+++ Strings/Easy/valid_anagram.cpp
@@ -0,0 +1,18 @@
+// Code to check if two given strings are anagrams
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i = 0;i<s.length();i++){
+            if(s[i] != t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
